63ed35d489f58bc57e2a3b90ff67bc70b3281e07
uservm <example@mail.com>
2025-06-10 19:33:22 +0300

    Add admin interface improvements and django-cleanup
    - api_auth/admin.py:
      * Implemented ProfileAdmin with custom display fields and avatar links
      * Added AvatarAdmin with optimized queries
      * Improved admin interface usability with search and ordering
    
    - api_auth/models.py:
      * Added __str__ methods for Avatar and Profile models
      * Set verbose names for Profile model
    
    - api_auth/views.py:
      * Removed manual avatar file deletion (handled by django-cleanup)
    
    - api_product/admin.py:
      * Created ProductAdmin with basic list configuration
    
    - settings.py:
      * Added django-cleanup to INSTALLED_APPS for automatic file cleanup
    
    - pyproject.toml:
      * Added django-cleanup package dependency

--- megano/api_product/admin.py
+++ megano/api_product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = "id", "title", "price", "count", "date",
+    list_display_links = "id", "title"
+    ordering = "id",
+    search_fields = "title", "price", "count"
