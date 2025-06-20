d32d2fbdb77296c2adb81414ab8ecc281f7ca918
uservm <example@mail.com>
2025-06-20 15:25:38 +0300

    Test workflow

--- megano/some_file.py
+++ megano/some_file.py
@@ -24,7 +24,7 @@ def example(param1, param2):  # Лишние пробелы - Black исправ
 
 
 result = (
-    some_object.method1()
+    some_object1.method1()
     .method2()
     .method3("очень длинный аргумент, который не влезает в 100 символов")
 )
